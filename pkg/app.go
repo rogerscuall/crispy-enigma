0d99fc25b4b318c2a09f1088f99cd89a38623d88
Roger Gomez <rgomez@presidio.com>
2024-01-12 01:57:10 -0500

    application struct.

---
+++ pkg/app.go
@@ -0,0 +1,26 @@
+package pkg
+
+import (
+	"github.com/netbox-community/go-netbox/v3/netbox/client"
+	"github.com/rogerscuall/crispy-enigma/models"
+)
+
+type Application struct {
+	Devices      []*models.Config
+	NetBoxclient *client.NetBoxAPI
+}
+
+func NewApplication() *Application {
+	//WARNING: this is an extremely important command, do not remove.
+	client.DefaultSchemes = []string{"https"}
+	return &Application{
+		Devices: make([]*models.Config, 0),
+	}
+}
+
+func (a *Application) AddDevice(device *models.Config) {
+	if a.Devices == nil {
+		a.Devices = make([]*models.Config, 0)
+	}
+	a.Devices = append(a.Devices, device)
+}
