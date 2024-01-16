5c91ef445d05820d3b425c1001be1007f10d2247
Roger Gomez <rgomez@presidio.com>
2024-01-16 16:50:44 -0500

    Adding the infoblox client.

--- pkg/app.go
+++ pkg/app.go
@@ -1,13 +1,15 @@
 package pkg
 
 import (
+	ibclient "github.com/infobloxopen/infoblox-go-client/v2"
 	"github.com/netbox-community/go-netbox/v3/netbox/client"
 	"github.com/rogerscuall/crispy-enigma/models"
 )
 
 type Application struct {
-	Devices      []*models.Config
-	NetBoxclient *client.NetBoxAPI
+	Devices        []*models.Config
+	NetBoxclient   *client.NetBoxAPI
+	InfobloxClient *ibclient.Connector
 }
 
 func NewApplication() *Application {
