44c699dd13dfeb0810f46a9416f3181aa8562124
Roger Gomez <rgomez@presidio.com>
2024-02-02 14:23:33 -0500

    doc update.

--- cmd/root.go
+++ cmd/root.go
@@ -48,13 +48,8 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "crispy-enigma",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Collection of scripts to use with AVD",
+	Long:  `Collection of scripts to use with AVD`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
