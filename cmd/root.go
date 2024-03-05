cfdb5b7e4fa48137e9144e49fc84030f7e217b77
Roger Gomez <rgomez@presidio.com>
2024-03-05 13:08:38 -0500

    Moving the function to the root command.

--- cmd/root.go
+++ cmd/root.go
@@ -43,6 +43,10 @@ var (
 	cvpUsername    string
 	cvpPassword    string
 	cvpToken       string
+	pbURL          string
+	pbUsername     string
+	pbPassword     string
+	verbose        bool
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -76,6 +80,9 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "enable verbose output")
+	rootCmd.AddCommand(cvpConfigCmd)
+	rootCmd.AddCommand(pbAccessInterfaceCmd)
 }
 
 // initConfig reads in config file and ENV variables if set.
