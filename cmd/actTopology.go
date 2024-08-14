02a910a3ee604cb5cfa2b8346235031fecb2c4c8
Roger Gomez <rgomez@presidio.com>
2024-08-14 11:26:45 -0400

    starting the work in act auto topology.

--- cmd/actTopology.go
+++ cmd/actTopology.go
@@ -4,18 +4,11 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net"
 	"os"
 
-	"github.com/apenella/go-ansible/v2/pkg/execute"
-	"github.com/apenella/go-ansible/v2/pkg/inventory"
-	"github.com/c-robinson/iplib"
+	"github.com/rogerscuall/crispy-enigma/internal/act"
 	mo "github.com/rogerscuall/crispy-enigma/models"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
@@ -61,158 +54,35 @@ func init() {
 	// actTopologyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// Define struct to represent the JSON data
-type InventoryData struct {
-	Meta MetaData `json:"_meta"`
-}
-
-func (i *InventoryData) GetHost() []string {
-	var hostnames []string
-	fmt.Println("Hosts information:")
-	for hostname, vars := range i.Meta.HostVars {
-		fmt.Printf("Hostname: %s\n", hostname)
-		for key, value := range vars {
-			fmt.Printf("  %s: %v\n", key, value)
-		}
-		hostnames = append(hostnames, hostname)
-	}
-	return hostnames
-}
-
-type MetaData struct {
-	HostVars map[string]HostVars `json:"hostvars"`
-}
-
-type HostVars map[string]interface{}
-
-// Struct definitions
-type ACTTopologyConfig struct {
-	Veos       DeviceDetails `yaml:"veos"`
-	Cvp        DeviceDetails `yaml:"cvp"`
-	Generic    DeviceDetails `yaml:"generic"`
-	ThirdParty DeviceDetails `yaml:"third-party"`
-	Nodes      []*Node       `yaml:"nodes"`
-	Links      []*Link       `yaml:"links"`
-}
-
-type DeviceDetails struct {
-	Password string `yaml:"password"`
-	Username string `yaml:"username"`
-	Version  string `yaml:"version"`
-}
-
-type Node struct {
-	Name     string   `yaml:"-"`
-	IPAddr   string   `yaml:"ip_addr"`
-	NodeType string   `yaml:"node_type"`
-	Ports    []string `yaml:"ports"`
-}
-
-type Link struct {
-	Connection []string `yaml:"connection"`
-}
-
 var (
 	// defaultPorts = []string{"Ethernet1-32"}
 	debug = false
 )
 
-// AddNode adds a new node to the ACTTopologyConfig
-func (a *ACTTopologyConfig) AddNode(nodeName, ipAddr string) error {
-	// Check if the node already exists
-	for _, n := range a.Nodes {
-		if n.Name == nodeName {
-			return fmt.Errorf("Node %s already exists", nodeName)
-		}
-	}
-	a.Nodes = append(a.Nodes, &Node{Name: nodeName, IPAddr: ipAddr, NodeType: "veos"})
-	return nil
-}
-
-func (n *Node) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var nodeMap map[string]interface{}
-	if err := unmarshal(&nodeMap); err != nil {
-		return err
-	}
-
-	for key, value := range nodeMap {
-		n.Name = key
-
-		nodeDetails := value.(map[interface{}]interface{})
-		n.IPAddr = nodeDetails["ip_addr"].(string)
-		n.NodeType = nodeDetails["node_type"].(string)
-
-		ports := nodeDetails["ports"].([]interface{})
-		for _, port := range ports {
-			portName := port.(string)
-			n.Ports = append(n.Ports, portName)
-		}
-	}
-
-	return nil
-}
-
-func (n Node) MarshalYAML() (interface{}, error) {
-	nodeDetails := map[string]interface{}{
-		"ip_addr":   n.IPAddr,
-		"node_type": n.NodeType,
-		"ports":     n.Ports,
-	}
-
-	return map[string]interface{}{
-		n.Name: nodeDetails,
-	}, nil
-}
-
 func actTopology() {
 
-	/*
-		TODO: We are reading info from the structured configuration files.
-		TODO: So we dont need to run the inventory command anymore.
-	*/
-	inventoryData, err := getInventoryData(debug, "../inventory.yml")
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	hostnames := inventoryData.GetHost()
-	if debug {
-		fmt.Println("Hostnames:", hostnames)
-	}
-	// Load the YAML data from a file
-	yamlData, err := os.ReadFile("topology.yml")
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	var config ACTTopologyConfig
-	err = yaml.Unmarshal([]byte(yamlData), &config)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	// Print the unmarshalled data
-	if debug {
-		fmt.Printf("%+v\n", config)
-	}
-	// Add a new node to the config with an unique IP address
-	config.AddIPToHosts(hostnames, "192.168.0.20")
 	files, err := getYmlFiles("intended/structured_configs")
 	if err != nil {
 		fmt.Println(err)
 	}
-	var configs []*mo.Config
+	var network mo.Network
+	var networkConfigs []*mo.Config
 	for _, file := range files {
 		fmt.Println("Working on file:", file)
 		c, err := mo.NewConfigFromYaml(file)
 		if err != nil {
 			cobra.CheckErr(err)
 		}
-		configs = append(configs, c)
+		networkConfigs = append(networkConfigs, c)
 	}
-	// Get all the ethernet interfaces from the structured configuration
+	network.Configs = networkConfigs
+	hostnames := network.GetHostnames()
+	if debug {
+		fmt.Println("Hostnames:", hostnames)
+	}
+	networkConnections := network.CleanNetworkConnections()
 	interfaceMap := make(map[string][]string)
-	for _, c := range configs {
+	for _, c := range networkConfigs {
 		var interfaces []string
 		if _, ok := interfaceMap[c.Hostname]; !ok {
 			interfaceMap[c.Hostname] = interfaces
@@ -222,25 +92,35 @@ func actTopology() {
 		}
 		interfaceMap[c.Hostname] = interfaces
 	}
+	// Load the YAML data from a file
+	yamlData, err := os.ReadFile("topology.yml")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	var actConfig act.TopologyConfig
+	err = yaml.Unmarshal([]byte(yamlData), &actConfig)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	// Print the unmarshalled data
+	if debug {
+		fmt.Printf("%+v\n", actConfig)
+	}
+	// Add a new node to the config with an unique IP address
+	actConfig.AddIPToHosts(hostnames, "192.168.0.20")
+
+	// Get all the ethernet interfaces from the structured configuration
+	
 
 	// networkInterfaces interfaces connected to other network devices in this fabric
-	networkInterfaces := make(map[string][]*mo.EthernetInterface)
-	for _, c := range configs {
-		networkInterfaces[c.Hostname] = NetworkInterfaces(c, hostnames)
-	}
-	linksMap := CreateLinkMap(networkInterfaces)
-	fmt.Println("Links Map")
-	for h, links := range linksMap {
-		fmt.Println("Hosts", h)
-		for _, l := range links {
-			fmt.Println(l)
-		}
-	}
-	config.AddPortsToNodes(interfaceMap)
-	// config.AddLinksToNodes(linksMap)
+
+	actConfig.AddPortsToNodes(interfaceMap)
+	actConfig.AddLinksToNodes(networkConnections)
 
 	// Output to a file
-	yamlData, err = yaml.Marshal(&config)
+	yamlData, err = yaml.Marshal(&actConfig)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
@@ -270,90 +150,57 @@ func actTopology() {
 
 // getInventoryData runs the ansible-inventory command and returns the InventoryData struct
 // Ansible is needed to run this command
-func getInventoryData(debug bool, inventoryPath string) (InventoryData, error) {
-	ansibleInventoryOptions := inventory.AnsibleInventoryOptions{
-		// Graph:     true,
-		List:      true,
-		Inventory: inventoryPath,
-		Vars:      true,
-		// Yaml:      true,
-	}
+// func getInventoryData(debug bool, inventoryPath string) (InventoryData, error) {
+// 	ansibleInventoryOptions := inventory.AnsibleInventoryOptions{
+// 		// Graph:     true,
+// 		List:      true,
+// 		Inventory: inventoryPath,
+// 		Vars:      true,
+// 		// Yaml:      true,
+// 	}
 
-	buff := new(bytes.Buffer)
+// 	buff := new(bytes.Buffer)
 
-	// Run the ansible-inventory command with options
-	inventoryCmd := inventory.NewAnsibleInventoryCmd(
-		inventory.WithPattern("all"),
-		inventory.WithInventoryOptions(&ansibleInventoryOptions),
-	)
+// 	// Run the ansible-inventory command with options
+// 	inventoryCmd := inventory.NewAnsibleInventoryCmd(
+// 		inventory.WithPattern("all"),
+// 		inventory.WithInventoryOptions(&ansibleInventoryOptions),
+// 	)
 
-	if debug {
-		fmt.Println("Test strings:", inventoryCmd.String())
-	}
+// 	if debug {
+// 		fmt.Println("Test strings:", inventoryCmd.String())
+// 	}
 
-	// Execute the command and write the output to the buffer
-	exec := execute.NewDefaultExecute(
-		execute.WithCmd(inventoryCmd),
-		execute.WithWrite(io.Writer(buff)),
-	)
+// 	// Execute the command and write the output to the buffer
+// 	exec := execute.NewDefaultExecute(
+// 		execute.WithCmd(inventoryCmd),
+// 		execute.WithWrite(io.Writer(buff)),
+// 	)
 
-	err := exec.Execute(context.TODO())
-	if err != nil {
-		return InventoryData{}, err
-	}
+// 	err := exec.Execute(context.TODO())
+// 	if err != nil {
+// 		return InventoryData{}, err
+// 	}
 
-	// Parse the JSON data
-	var inventoryData InventoryData
-	err = json.Unmarshal(buff.Bytes(), &inventoryData)
-	if err != nil {
-		return InventoryData{}, err
-	}
+// 	// Parse the JSON data
+// 	var inventoryData InventoryData
+// 	err = json.Unmarshal(buff.Bytes(), &inventoryData)
+// 	if err != nil {
+// 		return InventoryData{}, err
+// 	}
 
-	if debug {
-		prettyJSON, err := json.MarshalIndent(inventoryData, "", "  ")
-		if err != nil {
-			return InventoryData{}, err
-		}
+// 	if debug {
+// 		prettyJSON, err := json.MarshalIndent(inventoryData, "", "  ")
+// 		if err != nil {
+// 			return InventoryData{}, err
+// 		}
 
-		fmt.Println("Parsed JSON data:")
-		fmt.Println(string(prettyJSON))
-	}
+// 		fmt.Println("Parsed JSON data:")
+// 		fmt.Println(string(prettyJSON))
+// 	}
 
-	return inventoryData, nil
-}
-
-// AddIPToHosts adds a new node to the ACTTopologyConfig
-func (c *ACTTopologyConfig) AddIPToHosts(hostnames []string, firstIP string) {
-	ipInit := net.ParseIP(firstIP)
-	ipNext := iplib.NextIP(ipInit)
-	for _, hostname := range hostnames {
-		if err := c.AddNode(hostname, ipNext.String()); err != nil {
-			fmt.Println(err)
-		}
-		ipNext = iplib.NextIP(ipNext)
-	}
-}
-
-func (c *ACTTopologyConfig) AddPortsToNodes(interfaceMap map[string][]string) {
-	for _, node := range c.Nodes {
-		// avoid overwriting the ports if the node is not in the interfaceMap
-		if _, ok := interfaceMap[node.Name]; !ok {
-			continue
-		}
-		node.Ports = interfaceMap[node.Name]
-	}
-}
-
-/*
-	 AddLinksToNodes adds links to the nodes in the ACTTopologyConfig
-		linksMap should have this format:
-		{"node1": ["node2:port1", "node3:port2"], "node2": ["node1:port1"]}
-*/
-func (c *ACTTopologyConfig) AddLinksToNodes(linksMap map[string][]string) {
-	for hostname := range linksMap {
-		c.Links = append(c.Links, &Link{Connection: linksMap[hostname]})
-	}
-}
+// 	return inventoryData, nil
+// }
 
 // NonNetworkInterfaces returns a list list of interfaces that are not connected to a network device
 func NonNetworkInterfaces(m *mo.Config, hostnames []string) []*mo.EthernetInterface {
