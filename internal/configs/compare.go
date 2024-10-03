9b8fc64d68c1e0b068c375d303875d4d77a94215
Roger Gomez <rgomez@presidio.com>
2024-10-03 19:04:08 -0400

    Add CompareNetworkConfigs function to compare two network configurations

---
+++ internal/configs/compare.go
@@ -0,0 +1,8 @@
+package configs
+
+// CompareNetworkConfigs compares two network configurations and returns true if they are the same.
+// This configurations might be the same but in different order.
+func CompareNetworkConfigs(running, designed string) bool {
+
+}
+
